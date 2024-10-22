b1676e04bbeed6a0c835f38487bf84b5fc9ec288
Leyla <leylanaqiyeva940@gmail.com>
2024-10-22 23:38:36 +0400

    lesson2.task2

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int x=10;
+        int y=8;
+        x=x+y;  //10+8=18
+        y=x-y;   //18-8=10
+        x=x-y;    //  18-10=8
+        System.out.println("x="+x);
+        System.out.println("y="+y);
+    }
+}
